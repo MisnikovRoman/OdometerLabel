264a617791eaec2b97d46d2cce17e94cc7ea45b9
Misnikov Roman <misnikovroman@gmail.com>
2022-01-21 19:36:38 +0300

    Индикатор как счетчик пробега в машине

--- Shared/RollingNumberApp.swift
+++ Shared/RollingNumberApp.swift
@@ -1,17 +1,10 @@
-//
-//  RollingNumberApp.swift
-//  Shared
-//
-//  Created by Roman Misnikov on 21.01.2022.
-//
-
 import SwiftUI
 
 @main
 struct RollingNumberApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RollingNumbersTestScreen()
         }
     }
 }
