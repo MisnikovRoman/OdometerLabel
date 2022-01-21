6434dfa17c2457377017c0cd9b85f24e12f84d76
Misnikov Roman <misnikovroman@gmail.com>
2022-01-21 16:56:15 +0300

    Initial Commit

---
+++ Shared/RollingNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollingNumberApp.swift
+//  Shared
+//
+//  Created by Roman Misnikov on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct RollingNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
